bdff8fdcc991b230991b777f68b076b5fad72e2e
Ignatella <ignatellasecond@gmail.com>
2024-03-26 14:15:19 +0100

    Add: buffor

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
-int main(int argc, char *argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+
+void memory_leak() {
+    int *ptr = (int *)malloc(sizeof(int));
+    if (ptr == NULL) {
+        printf("Memory allocation failed.\n");
+        return;
+    }
+
+    *ptr = 10;
 
 }
+
+int main() {
+    memory_leak();
+
+    printf("Program executed successfully.\n");
+    return 0;
+}
+
